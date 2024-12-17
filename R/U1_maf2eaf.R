39f74c56db256e6a6d2abf7eee414d0af078fe3d
L Liang <145899484+gwendyun@users.noreply.github.com>
2024-12-17 14:08:12 +0000

    404?

--- R/U1_maf2eaf.R
+++ R/U1_maf2eaf.R
@@ -1,57 +1,51 @@
 #' @title U1_maf2eaf
 #' @description Convert MAF to EAF in a dataframe, checking for and renaming relevant columns.
 #'
-#' @param df A dataframe containing MAF or EAF columns.
+#' @param dat A dataframe containing MAF or EAF columns.
 #'
 #' @return A dataframe with EAF column, preserving the original MAF column.
 #' @export
 #'
 #' @examples
-#' df <- U1_maf2eaf(df)
+#' dat <- U1_maf2eaf(dat)
 #'
 #'
-U1_maf2eaf <- function(df) {
-  require(dplyr)
-
-  rename_col <- function(df ,
-                         patterns ,
-                         format ){
-    B <- colnames( df )
-
-    for(i in 1:length(B)){
+U1_maf2eaf <- function(dat) {
+  suppressMessages(require('dplyr'))
+  suppressMessages(require('progress'))
+  
+  rename_col <- function(df, patterns, format) {
+    B <- colnames(df)
+    for (i in 1:length(B)) {
       col <- B[i]
-      for( y in 1:length(patterns) ){
-        if( tolower(patterns[y]) == tolower(B[i])){
-
+      for (y in 1:length(patterns)) {
+        if (tolower(patterns[y]) == tolower(B[i])) {
           if (!B[i] == format) {
             cat("Replacing", B[i], "with", format, "\n")
           }
-
           B[i] <- format
         }
       }
     }
-
     colnames(df) <- B
-
     if (length(unique(colnames(df))) != length(colnames(df))) {
       stop(paste(format, "column name is duplicated, please use `help(U1_Clean_data)` to check the documentation and provide only one!"), call. = FALSE)
     }
-
     return(df)
   }
-
-  if(is.data.frame(df)){ df = as.data.frame(df) }
-
+  
+  if (is.data.frame(dat)) { dat = as.data.frame(dat) }
+  
   # Check for MAF columns and rename if present
-  if (any(c("MinorAlleleFrequency", "minor_allele_frequency", "MAF", "Freq", "freq", "alt_freq", 'freq_minor', 'Freq_Minor_Allele', "maf") %in% colnames(df))) {
-    df <- rename_col(df, patterns = c("MinorAlleleFrequency", "minor_allele_frequency", "MAF", "Freq", "freq", "alt_freq", 'freq_minor', 'Freq_Minor_Allele', "maf"), format = "maf")
+  if (any(c("MinorAlleleFrequency", "minor_allele_frequency", "MAF", "Freq", "freq", "alt_freq", 'freq_minor', 'Freq_Minor_Allele', "maf") %in% colnames(dat))) {
+    dat <- rename_col(dat, patterns = c("MinorAlleleFrequency", "minor_allele_frequency", "MAF", "Freq", "freq", "alt_freq", 'freq_minor', 'Freq_Minor_Allele', "maf"), format = "maf")
+    dat <- rename_col(dat, patterns = c("snp", "rsid", "rsids", "snpid", "rnpid", "rs", "variant_id"), format = "SNP")
     
-    # if maf exist, then add eaf_ref from EU 1000G 
-    if ('maf' %in% colnames(df)) {
-
-      message ('1000G use EU pop as default')
-
+    # if maf exists, then add eaf_ref from EU 1000G 
+    if ('maf' %in% colnames(dat)) {
+      message('1000G use EU pop as default')
+      
+      # Function to fetch EAF from Ensembl for each SNP
       find_eaf <- function(SNP, pop = "EUR") {
         server <- "http://rest.ensembl.org/variation/Homo_sapiens/%s?content-type=application/json;pops=1"
         server <- sprintf(server, SNP)
@@ -66,26 +60,58 @@ U1_maf2eaf <- function(df) {
         }
         return(eaf.final)
       }
-
-      df$eaf_ref <- sapply(df$SNP, find_eaf)
-
       
-      if (all(df$maf < 0.5, na.rm = TRUE) & all(df$eaf_ref < 0.5, na.rm = TRUE)) {
-        df$eaf <- df$maf
-        message('compare maf with eaf_ref. both are less than 0.5. eaf=maf')
+      # Function to fill eaf_ref column using find_eaf for each SNP
+      fill_eaf_ref <- function(dat, pop = "EUR") {
+        # Find the rows where eaf_ref is NA
+        index <- which(is.na(dat$eaf_ref))
+        
+        if (length(index) > 0) {
+          message(paste0("There are ", length(index), " SNPs without EAF in eaf_ref."))
+        }
+        
+        # Get the unique SNPs that need to be filled
+        index_SNP <- unique(dat$SNP[index])
+        
+        # Create a progress bar for the query process
+        pb <- progress::progress_bar$new(total = length(index_SNP))
+        
+        # Iterate through the SNPs and fill in the eaf_ref column
+        for (i in 1:length(index_SNP)) {
+          dat$eaf_ref[which(dat$SNP == index_SNP[i])] <- find_eaf(index_SNP[i], pop)
+          pb$tick()
+        }
+        
+        # If still NA after attempting to fill, fill with 0.5
+        index <- which(is.na(dat$eaf_ref))
+        if (length(index) > 0) {
+          message(paste0("There are still ", length(index), " SNPs without EAF in eaf_ref. Filling with 0.5."))
+          dat$eaf_ref[index] <- 0.5
+        } else {
+          message("All SNPs in eaf_ref have been filled.")
+        }
+        
+        return(dat)
+      }
+      
+      # Call the fill_eaf_ref function
+      dat <- fill_eaf_ref(dat, pop = "EUR")
+      
+      # Handle assignment of eaf column
+      if (all(dat$maf < 0.5, na.rm = TRUE) & all(dat$eaf_ref < 0.5, na.rm = TRUE)) {
+        dat$eaf <- dat$maf
+        message('Compare maf with eaf_ref: both are less than 0.5. eaf=maf')
       } else {
-        message('compare maf with eaf_ref. effect_allele is a major allele, df$eaf = 1 - df$maf')
-        return(df)
+        message('Compare maf with eaf_ref: effect allele is a major allele, eaf = 1 - maf')
+        dat$eaf <- 1 - dat$maf
       }
     }
-  } else if (any(c("eaf", "FREQ", "af_alt", "FREQ1", "effect_allele_frequency", "Freq_Tested_Allele", "Alternate.Allele.Frequency") %in% colnames(df))) {
-    df <- rename_col(df, patterns = c("eaf", "FREQ", "af_alt", "FREQ1", "effect_allele_frequency", "Freq_Tested_Allele", "Alternate.Allele.Frequency"), format = "eaf")
+  } else if (any(c("eaf", "FREQ", "af_alt", "FREQ1", "effect_allele_frequency", "Freq_Tested_Allele", "Alternate.Allele.Frequency") %in% colnames(dat))) {
+    dat <- rename_col(dat, patterns = c("eaf", "FREQ", "af_alt", "FREQ1", "effect_allele_frequency", "Freq_Tested_Allele", "Alternate.Allele.Frequency"), format = "eaf")
   } else {
-    # no eaf and maf 
-    message('No MAF and eaf column found. it is really important to have this. this can not be added just using 1000G')
+    message('No MAF and eaf column found. It is really important to have this. This cannot be added just using 1000G.')
     return(NULL)
   }
-
-  # Return the dataframe with the new EAF column, preserving the original MAF column
-  return(df)
+  
+  return(dat)
 }
